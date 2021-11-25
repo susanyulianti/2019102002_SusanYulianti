5d294ef299f158a2f99d27760ba7b74ca0ed588b
susanyulianti <susanyulianty17@gmail.com>
2021-11-25 07:09:48 +0700

    coba

---
+++ app/src/main/java/com/example/a2019102002_susanyulianti/Scroll.java
@@ -0,0 +1,14 @@
+package com.example.a2019102002_susanyulianti;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Scroll extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scroll);
+    }
+}
